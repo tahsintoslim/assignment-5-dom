194f0892fe5032919ff040db14a221e372ad0baa
tahsintoslim <tahsintoslim10@gmail.com>
2024-02-21 22:47:55 +0600

    fourth

--- scripts/index.js
+++ scripts/index.js
@@ -44,16 +44,16 @@ function BuyTickets(){
 
 
 
-  function getARandomAlphabet(){
-    const alphabetString ='A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4I1I2I3I4J1J2J3J4';
-    const alphabets = alphabetString.split('');
-    const randomNumber = Math.random()*39;
-    const index = Math.round(randomNumber);
-    const alphabet = alphabets[index];
+//   function getARandomAlphabet(){
+//     const alphabetString ='A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4I1I2I3I4J1J2J3J4';
+//     const alphabets = alphabetString.split('');
+//     const randomNumber = Math.random()*39;
+//     const index = Math.round(randomNumber);
+//     const alphabet = alphabets[index];
 
-    return alphabets;
-    const currentAlphabetElement = document.getElementById(current-alphabet);
-    currentAlphabetElement.innerText = alphabet;
+//     return alphabets;
+//     const currentAlphabetElement = document.getElementById(current-alphabet);
+//     currentAlphabetElement.innerText = alphabet;
 
 
-}
+// }
