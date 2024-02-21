f154a33d2c23fa1fbff5873fb7f977abcb2fd564
tahsintoslim <tahsintoslim10@gmail.com>
2024-02-21 22:34:06 +0600

    second

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,22 @@
 
 
 
-function getARandomAlphabet(){
-    const alphabetString ='A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4I1I2I3I4J1J2J3J4';
+// function getARandomAlphabet(){
+//     const alphabetString ='A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4I1I2I3I4J1J2J3J4';
 
-    const alphabets = alphabetString.split('');
+//     const alphabets = alphabetString.split('');
+const randomNumber = Math.random()*39;
+const index = Math.round(randomNumber);
+const alphabet = alphabets[index];
+return alphabets;
+//     const alphabet = getARandomAlphabet()
+//     
+//    
+//     const alphabet = alphabets[index];
+//     //console.log(index, alphabets);
+//     return alphabets;
 
-    const alphabet = getARandomAlphabet()
-    const randomNumber = Math.random()*39;
-    const index = Math.round(randomNumber);
-    const alphabet = alphabets[index];
-    //console.log(index, alphabets);
-    return alphabets;
-
-}
+// }
 
 function BuyTickets(){
    
@@ -26,4 +29,16 @@ function BuyTickets(){
     continueGame()
   
   }
+  function getARandomAlphabet(){
+    const alphabetString ='A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4I1I2I3I4J1J2J3J4';
+    const alphabets = alphabetString.split('');
+    const randomNumber = Math.random()*39;
+    const index = Math.round(randomNumber);
+    const alphabet = alphabets[index];
 
+    return alphabets;
+    const currentAlphabetElement = document.getElementById(current-alphabet);
+    currentAlphabetElement.innerText = alphabet;
+
+
+}
