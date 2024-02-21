01e1449917421525d6e99e20e3a439bf872d1f23
tahsintoslim <tahsintoslim10@gmail.com>
2024-02-21 22:04:54 +0600

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+
+
+
+
+function getARandomAlphabet(){
+    const alphabetString ='A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4I1I2I3I4J1J2J3J4';
+
+    const alphabets = alphabetString.split('');
+
+    const alphabet = getARandomAlphabet()
+    const randomNumber = Math.random()*39;
+    const index = Math.round(randomNumber);
+    const alphabet = alphabets[index];
+    //console.log(index, alphabets);
+    return alphabets;
+
+}
+
+function BuyTickets(){
+   
+    const homeSection = document.getElementById('home-screen');
+    homeSection.classList.add('hidden');
+   
+    const playgroundSection = document.getElementById('play-ground');
+    playgroundSection.classList.remove('hidden');
+    continueGame()
+  
+  }
+
