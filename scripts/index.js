c9eaf67c424c4d73456b0d84435228eb5d289db5
tahsintoslim <tahsintoslim10@gmail.com>
2024-02-21 22:42:09 +0600

    therd

--- scripts/index.js
+++ scripts/index.js
@@ -6,10 +6,10 @@
 //     const alphabetString ='A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4I1I2I3I4J1J2J3J4';
 
 //     const alphabets = alphabetString.split('');
-const randomNumber = Math.random()*39;
-const index = Math.round(randomNumber);
-const alphabet = alphabets[index];
-return alphabets;
+// const randomNumber = Math.random()*39;
+// const index = Math.round(randomNumber);
+// const alphabet = alphabets[index];
+// return alphabets;
 //     const alphabet = getARandomAlphabet()
 //     
 //    
@@ -19,16 +19,31 @@ return alphabets;
 
 // }
 
-function BuyTickets(){
+// function BuyTickets(){
    
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+   
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+//     // continueGame()
+  
+//   }
+
+function BuyTickets(){
+    //console.log('Tickets start now');
     const homeSection = document.getElementById('home-screen');
     homeSection.classList.add('hidden');
-   
+    //console.log(homeSection)
     const playgroundSection = document.getElementById('play-ground');
     playgroundSection.classList.remove('hidden');
-    continueGame()
+    //console.log('playgroundSection.classList')
+    //continueSell()
   
   }
+
+
+
   function getARandomAlphabet(){
     const alphabetString ='A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4I1I2I3I4J1J2J3J4';
     const alphabets = alphabetString.split('');
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,8 +1,8 @@
-function hiddenElementById( elementId){
-    const element = document.getElementById(elementId);
-    element.classList.add('hiddent');
-}
-function showElementById(){
-    const element = document.getElementById(elementId);
-    element.classList.remove('hiddent');
-}
+// function hiddenElementById( elementId){
+//     const element = document.getElementById(elementId);
+//     element.classList.add('hiddent');
+// }
+// function showElementById(){
+//     const element = document.getElementById(elementId);
+//     element.classList.remove('hiddent');
+// }
